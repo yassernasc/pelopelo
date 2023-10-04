b19db6937c4455837ff5d0085e8adbcbb2108d0d
Yasser Nascimento <dev@yasser.email>
2023-10-04 14:46:23 -0300

    update footer

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -4,26 +4,33 @@ import styles from "./Footer.module.css";
 import { TempLogo } from "@/components";
 
 export const Footer = () => (
-  <section class="colored-section" id="footer">
-    <div class={c("colored-section-content", styles.content)}>
-      <TempLogo />
-      <div class={styles.verticalLine}></div>
-      <div class={styles.social}>
-        <div class={styles.icons}>
-          <a target="_blank" href="https://www.instagram.com/pelopelocg">
+  <>
+    <section class="colored-section" id="footer">
+      <div class={c("colored-section-content", styles.content)}>
+        <TempLogo />
+        <div class={styles.verticalLine}></div>
+        <div class={styles.social}>
+          <div class={styles.icons}>
+            <a target="_blank" href="https://www.instagram.com/pelopelocg">
+              <div class={styles.iconContainer}>
+                <img src="instagram.png" />
+              </div>
+            </a>
             <div class={styles.iconContainer}>
-              <img src="instagram.png" />
+              <img src="zap.png" />
             </div>
-          </a>
-          <div class={styles.iconContainer}>
-            <img src="zap.png" />
+            <a target="_blank" href="mailto:contato@pelopelo.org">
+              <div class={styles.iconContainer}>
+                <img src="mail.png" />
+              </div>
+            </a>
           </div>
-          <div class={styles.iconContainer}>
-            <img src="mail.png" />
-          </div>
+          <span>contato@pelopelo.org</span>
         </div>
-        <span>PeloPelo - 2023</span>
       </div>
-    </div>
-  </section>
+    </section>
+    <section class={c("colored-section", styles.end)}>
+      <p>PeloPelo - Fazendo a diferença na causa animal - 2023</p>
+    </section>
+  </>
 );
