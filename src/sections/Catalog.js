67c97e2043cdbbb8f6b9b03934298e4989ab4cde
Yasser Nascimento <dev@yasser.email>
2023-10-20 18:57:54 -0300

    shuffle pets

--- src/sections/Catalog.js
+++ src/sections/Catalog.js
@@ -1,3 +1,4 @@
+import shuffle from "just-shuffle";
 import { getAnimals } from "@/services/supabase";
 import { getImages } from "@/services/cloudinary";
 import { CatalogComponent } from "@/components";
@@ -8,6 +9,7 @@ export const Catalog = async () => {
 
   const getImage = (pet) => images.find((i) => i.id == pet.id).url;
   pets = pets.map((p) => ({ ...p, image: getImage(p) }));
+  pets = shuffle(pets);
 
   return <CatalogComponent pets={pets} />;
 };
